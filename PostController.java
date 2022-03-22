345e7f184f9432a67c6526bc1b13add9e80b9a32
ManikantaDodda <88262364+ManikantaDodda@users.noreply.github.com>
2022-03-22 17:13:24 +0530

    Update PostController.java

--- PostController.java
+++ PostController.java
@@ -1,4 +1,4 @@
-package com.spacey.insta.user.post;
+package org.codejudge.sb.controller;
 
 import java.util.List;
 
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spacey.insta.exception.UserNotFoundException;
-import com.spacey.insta.response.StatusResponse;
+
 
 @RestController
 @RequestMapping("api/")
@@ -25,30 +24,18 @@ public class PostController {
 	@Autowired
 	PostService postService;
 
-	@PostMapping("/create-post/{username}")
+	@PostMapping("/create/{username}")
 	public Post createPost(@RequestBody Post post, @PathVariable("username") String username) {
 		return postService.createPost(post, username);
 	}
 
-	@GetMapping("/all-posts/{username}")
+	@GetMapping("/friends/{username}")
 	public List<Post> getAllPosts(@PathVariable("username") String username) {
 		return postService.getAllPosts(username);
 	}
 
-	@PutMapping("/post/{pid}/upvote")
-	public StatusResponse upvotePost(@PathVariable("pid") Long pid) {
-		return postService.upvotePost(pid);
-	}
 	
-	@ExceptionHandler(UserNotFoundException.class) // (optional to list exceptions here) takes an exception or a
-													// list of exceptions as an argument that
-													// we want to handle in the defined method
-	@ResponseStatus(code = HttpStatus.BAD_REQUEST) // Also, the annotation @ResponseStatus(HttpStatus.NOT_FOUND) on the
-													// handler method is not required as the HTTP status passed into the
-													// ResponseEnity will take precedence, but we have kept it anyway
-													// for the same readability reasons.
-	public ResponseEntity<StatusResponse> handleUserNotFoundException(UserNotFoundException exception) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new StatusResponse("failure", exception.getMessage()));
+	
 	}
 
 }
