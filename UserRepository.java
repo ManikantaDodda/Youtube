f44e8d5933ad3a92286eb0e90286e50f0e0f9cbf
ManikantaDodda <88262364+ManikantaDodda@users.noreply.github.com>
2022-03-22 17:15:21 +0530

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -1,4 +1,4 @@
-package com.spacey.insta.user;
+package org.codejudge.sb.controller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
